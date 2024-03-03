01cdbd005b71fde3b9009c22346c1457fc23f1d6
PracticeMedicine <102203580+PracticeMedicine03@users.noreply.github.com>
2024-03-04 03:45:12 +0800

    ye

--- FCLauncher.Beta/App.xaml.cs
+++ FCLauncher.Beta/App.xaml.cs
@@ -21,14 +21,66 @@ namespace FCLauncher.Beta
 
         static string[] args = Environment.GetCommandLineArgs();
 
+        private static UpdateManager mgr;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             Application.Current.DispatcherUnhandledException += CrashHandler;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
             base.OnStartup(e);
 
             if (args.Contains("--console") || args.Contains("-con"))
                 AllocConsole();
+
+            Task.Run(async () =>
+            {
+                using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/PracticeMedicine03/FCLauncher"))
+                {
+                    try
+                    {
+                        Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("[FCLAUNCHER SQURRIEL]: Checking for updates");
+                        var updateInfo = await mgr.Result.CheckForUpdate();
+
+                        if (updateInfo.ReleasesToApply.Any())
+                        {
+                            var versionCount = updateInfo.ReleasesToApply.Count;
+                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineBlue($"[FCLAUNCHER SQUIRREL]: {versionCount} update(s) found.");
+
+                            var versionWord = versionCount > 1 ? "versions" : "version";
+                            var message = new StringBuilder().AppendLine($"FCLauncher is {versionCount}").
+                                                              AppendLine("If you choose to update, changes won't take affect until FCLauncher is restarted.").
+                                                              AppendLine("Would you like to download and install them?").
+                                                              ToString();
+                            var result = MessageBox.Show(message, "FCLauncher Update", MessageBoxButton.YesNo);
+                            if (result != MessageBoxResult.Yes)
+                            {
+                                Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("Update declined by user.");
+                                return;
+                            }
+
+                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("[FCLAUNCHER SQUIRREL]: Downloading update...");
+                            var updateResult = await mgr.Result.UpdateApp();
+                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue($"[FCLAUNCHER SQUIRREL]: Download complete. Version {updateResult.Version} will take effect when FCLauncher is restarted");
+
+                            GC.WaitForFullGCComplete();
+                        }
+                        else
+                        {
+                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("[FCLAUNCHER SQUIRREL]: There are no updates.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineWarning(3, $"[FCLAUNCHER SQUIRREL]: There is an issue while trying to update.\nMessage:\n{ex.InnerException}\n{ex.Message}\n{ex.StackTrace}");
+                    }
+                }
+            });
+        }
+
+        private void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            mgr.Dispose();
         }
 
         protected void CrashHandler(object sender, DispatcherUnhandledExceptionEventArgs e)
--- FCLauncher.Beta/MainWindow.xaml.cs
+++ FCLauncher.Beta/MainWindow.xaml.cs
@@ -36,51 +36,6 @@ namespace FCLauncher.Beta
         {
             InitializeComponent();
 
-            Task.Run(async () =>
-            {
-                using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/PracticeMedicine03/FCLauncher"))
-                {
-                    try
-                    {
-                        Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("[FCLAUNCHER SQURRIEL]: Checking for updates");
-                        var updateInfo = await mgr.Result.CheckForUpdate();
-
-                        if (updateInfo.ReleasesToApply.Any())
-                        {
-                            var versionCount = updateInfo.ReleasesToApply.Count;
-                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineBlue($"[FCLAUNCHER SQUIRREL]: {versionCount} update(s) found.");
-
-                            var versionWord = versionCount > 1 ? "versions" : "version";
-                            var message = new StringBuilder().AppendLine($"FCLauncher is {versionCount}").
-                                                              AppendLine("If you choose to update, changes won't take affect until FCLauncher is restarted.").
-                                                              AppendLine("Would you like to download and install them?").
-                                                              ToString();
-                            var result = MessageBox.Show(message, "FCLauncher Update", MessageBoxButton.YesNo);
-                            if (result != MessageBoxResult.Yes)
-                            {
-                                Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("Update declined by user.");
-                                return;
-                            }
-
-                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("[FCLAUNCHER SQUIRREL]: Downloading update...");
-                            var updateResult = await mgr.Result.UpdateApp();
-                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue($"[FCLAUNCHER SQUIRREL]: Download complete. Version {updateResult.Version} will take effect when FCLauncher is restarted");
-                        }
-                        else
-                        {
-                            Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineDarkBlue("[FCLAUNCHER SQUIRREL]: There are no updates.");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Lambdagon.FCLauncher.Core.CommandLine.LauncherConsole.WriteLineWarning(3, $"[FCLAUNCHER SQUIRREL]: There is an issue while trying to update.\nMessage:\n{ex.Message}");
-                    }
-                }
-
-                GC.WaitForFullGCComplete();
-
-            });
-
             lua = new Lua();
 
             if(Directory.Exists(Steam.getSourcemodsFolder() + "/fc"))
